75a09950266f82144f64a49c26af314f4730e37b
diazGeoff <diazgeoff93@gmail.com>
2014-07-23 18:26:41 +0800

    Refactor Code

--- harvest-directory.js
+++ harvest-directory.js
@@ -1,14 +1,3 @@
-var configurationParser = function configurationParser(dataToParse){
-    dataToParse = dataToParse.replace(/\n/gm, '');
-    var commands = dataToParse.match(/\/\*:.+\*\//m);
-    var moduleObject = (commands ? commands[0] : "").match(/@module-configuration:(.+)@end-module-configuration/m);
-    var parsedData = JSON.parse(moduleObject ? moduleObject[1] : "{}");
-    if(parsedData){
-        return parsedData;
-    }
-    return false;
-}
-
 var harvestDirectory = function harvestDirectory(directory, harvester) {
     arrayOfFiles = traverseDirectory(directory);
     arrayOfTestFiles = [];
@@ -24,4 +13,3 @@ var traverseDirectory = require("../traverse-directory/traverse-directory");
 var arrayOfFiles, arrayOfTestFiles;
 
 ( module || {} ).exports.harvestDirectory = harvestDirectory;
-( module || {} ).exports.configurationParser = configurationParser;
